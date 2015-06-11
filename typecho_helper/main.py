e7184dd496e97466903ba87b41f169e301c5ee85
aneasystone <aneasystone@gmail.com>
2015-06-11 18:07:00 +0800

    update db

--- typecho_helper/main.py
+++ typecho_helper/main.py
@@ -33,15 +33,62 @@ def get_all_posts():
         results = cur.fetchall()
         for r in results:
             p = Post()
+            p.id = r[0]
             p.title = r[1]
             p.content = r[5]
             posts.append(p)
+            
+        conn.commit()
+        cur.close()
+        conn.close()
             
     except MySQLdb.Error,e:
         print "Mysql Error %d: %s" % (e.args[0], e.args[1])
     return posts
 
-def handle_post(post):
+def update_post(post):
+
+    try:
+        conn = MySQLdb.connect(host=configs.host,user=configs.user,passwd=configs.pwd,port=configs.port,charset='utf8')
+        cur = conn.cursor()
+        conn.select_db(configs.db)
+        
+        sql = 'update typecho_contents set `text` = %s where cid = %d' % post.content_md, post.id
+        cur.execute(sql)
+
+        conn.commit()
+        cur.close()
+        conn.close()
+            
+    except MySQLdb.Error,e:
+        print "Mysql Error %d: %s" % (e.args[0], e.args[1])    
+    return
+
+def convert_posts_to_markdown(posts):
+    
+    for p in posts:
+        if p.content:
+            # if content starts with <!--markdown-->
+            # there is no need to convert
+            pre = '<!--markdown-->'
+            if p.content.startswith(pre):
+                p.content_md = p.content
+            else:
+                p.content_md = pre + html2text.html2text(p.content)
+            
+    return posts
+
+def save_posts(posts):
+    
+    for p in posts:
+        if p.content:
+            print p.title
+            save_post(p)
+            #update_post(p)
+            
+    return
+
+def save_post(post):
     
     # save to html file
     htmlname = 'files/' + post.title + '.html'
@@ -53,7 +100,7 @@ def handle_post(post):
     
     # save to markdown file
     mdname = 'files/' + post.title + '.md'
-    md = html2text.html2text(post.content)
+    md = post.content_md
     #t = open(mdname, "wb")
     t = codecs.open(mdname, 'wb', 'utf-8');
     t.write(md)
@@ -66,11 +113,8 @@ def main():
         os.makedirs('files')    
     
     posts = get_all_posts()
-    for p in posts:
-        print p.title
-        #print p.content
-        if p.content:
-            handle_post(p)
+    posts = convert_posts_to_markdown(posts)
+    save_posts(posts)
 
     return
     
